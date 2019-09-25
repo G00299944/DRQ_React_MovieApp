ca7103b2522c18e6c3865a676db5e6118de0c59e
DARREN BUTLER - STUDENT <G00299944@gmit.ie>
2019-09-25 10:34:28 +0100

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <div className="Footer">
+        <h1>My Footer in another component</h1>
+      </div>
+    );
+  }
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="Header">
+        <h1>My Header in another component</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
